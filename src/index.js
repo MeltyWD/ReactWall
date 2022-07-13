3015ff68cbe650a13afbcb4e48c678fb224078a1
Melty <meltygd@gmail.com>
2022-07-13 22:00:12 +0300

    refactor index for darkMode

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ToggleColorMode from "./components/ToggleColorMode";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
-
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline /> {/* normalize.css */}
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <CssBaseline /> {/* normalize.css */}
+      <ToggleColorMode /> {/* DarkMode Middleware */}
+    </BrowserRouter>
   </React.StrictMode>
 );
