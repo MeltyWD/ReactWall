7a99aff2e31848b5a333017f3bc94eb74c686c2b
Melty <meltygd@gmail.com>
2022-07-13 22:02:30 +0300

    feat ToggleColorMode

---
+++ src/components/ToggleColorMode/index.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import App from "../../App";
+import ColorModeContext from "../../contexts/ColorModeContext";
+
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
